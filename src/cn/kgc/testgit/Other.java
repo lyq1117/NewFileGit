833ea3443c71972d6bccc61bdd2359fd5bb2778d
lyq1117 <610069623@qq.com>
2018-11-12 21:49:59 +0800

    do th. in Ohter.main() and TestGit.main()

--- src/cn/kgc/testgit/Other.java
+++ src/cn/kgc/testgit/Other.java
@@ -1,5 +1,7 @@
 package cn.kgc.testgit;
 
 public class Other {
-
+	public static void main(String[] args) {
+		System.out.println("Other main()");
+	}
 }
--- src/cn/kgc/testgit/TestGit.java
+++ src/cn/kgc/testgit/TestGit.java
@@ -2,6 +2,7 @@ package cn.kgc.testgit;
 
 public class TestGit {
 	public static void main(String[] args) {
+		System.out.println("TestGit main()");
 		System.out.println("hello git");
 	}
 }
